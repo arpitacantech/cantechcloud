7564a72c9dfbdeb4c4d41ed83eb898ed3d7156d5
arpitacantech <arpita.cantech@gmail.com>
2025-12-03 12:24:45 +0530

    update8

---
+++ app/paas/page.tsx
@@ -0,0 +1,14 @@
+
+import Navbar from "../components/Navbar";
+import HeroSection from "../paas/components/HeroSection";
+export default function paas() {
+  return (
+  <main >
+    <Navbar />
+  <HeroSection />
+  
+</main>
+
+
+  );
+}
